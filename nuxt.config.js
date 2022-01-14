9442a81426f3bbb8565adb4ecbf1de6e6ad77785
nxx <luciano.meniconi.r@gmail.com>
2022-01-14 03:47:49 -0300

    readme updated

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'DeathDance',
+    title: 'Dead Dance',
     htmlAttrs: {
       lang: 'en',
     },
